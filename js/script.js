fee96739a57ae42b0f669c422f59820298a39233
laetitia123 <uwizelaetitia@gmail.com>
2019-08-03 21:53:14 +0200

    checkout button added

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ Pizza.prototype.fullFilled = function() {
        
        var cost1 = 1000;
         console.log(cost1);
+        
       } else if ($("select#size").val() === "Medium") {
        var cost1 = 2000;
       } else if ($("select#size").val()=== "Large") {
@@ -69,7 +70,7 @@ Pizza.prototype.fullFilled = function() {
        
         $("ul#pizaa").append("<li><span class='contact'>" + newPrice + "</span></li>");
 
-        $("#show-contact").show();
+        $("#show-chosen").show();
 
         $(".pizza-size").text( newPizza.sizeName);
         
@@ -84,26 +85,42 @@ Pizza.prototype.fullFilled = function() {
   
         $("#delivery").click(function() {
           $("#gowith").show()
+          $(".form3").show()
           if ( $("select#size").val()=== "Small") {
             var price=1000;
             $(".price").text(price);
             console.log(price);
           }
            else if ( $("select#size").val()=== "Medium") {
-            var price=1500;
-            $(".price").text(price);
-            console.log(price);
+            var price2=1500;
+            $(".price").text(price2);
+            console.log(price2);
           }
          else  if ( $("select#size").val()=== "Large") {
-          var price=1800;
-          $(".price").text(price);
-          console.log(price);
+          var price3=1800;
+          $(".price").text(price3);
+          console.log(price3);
           }
           else{}
         });
       });    
  
-   
+      $(document).ready(function() {
+  
+        $("#checkout").click(function() {
+          if ( $("select#size").val()=== "Small"){
+            var total1=parseInt(cost1)+parseInt(price);
+            alert(total1);
+          }
+         else if ( $("select#size").val()=== "medium"){
+            var total1=parseInt(cost1)+parseInt(price2);
+            alert(total1);
+          }
+        
+          
+        });
+          
+      });    
 });
  resetFields();
   });
