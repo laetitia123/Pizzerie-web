ac8db20ca51e202e80e6762ceafdc7f5f7ecd8c2
laetitia123 <uwizelaetitia@gmail.com>
2019-08-05 21:44:04 +0200

    trying to slide images

--- js/script.js
+++ js/script.js
@@ -1,164 +1,362 @@
 
   
-  function Pizza(size, crust, topping,number) {
-    this.sizeName= size;
-    this.crustName= crust;
-    this.toppingName= topping;
-    this.numberName= number;
-  }
+//   function Pizza(size, crust, topping,number) {
+//     this.sizeName= size;(".pizza-size").text( newPizza.sizeName);
+        
+//     $(".pizza-crust").text( newPizza.crustName);
+//     $(".pizza-topping").text( newPizza.toppingName);
+//     $(".pizza-number").text( newPizza.numberName);
+//     this.crustName= crust;
+//     this.toppingName= topping;
+//     this.numberName= number;
+//   }
  
-Pizza.prototype.fullFilled = function() {
-    return this.sizeName+ " " + this.crustName+ "" + this.toppingName+"" +this.numberName;
-  }
+// Pizza.prototype.fullFilled = function() {
+//     return this.sizeName+ " " + this.crustName+ "" + this.toppingName+"" +this.numberName;
+//   }
  
     
-  function resetFields() {
-    $("input#size").val("");
-    $("input#crust").val("");
-    $("input#topping").val("");
+//   function resetFields() {
+//     $("input#size").val("");
+//     $("input#crust").val("");
+//     $("input#topping").val("");
     
+// }
+//   $(document).ready(function() {
+  
+//     $("#order").click(function() {
+       
+  
+    
+//     $("form#formid").submit(function(event) {
+//       event.preventDefault();
+   
+//       var inputtedSize = $("select#size").val();
+//       if ( $("select#size").val()=== "Small") {
+       
+       
+//        var cost1 = 1000;
+//         console.log(cost1);
+        
+//       } else if ($("select#size").val() === "Medium") {
+//        var cost1 = 2000;
+//       } else if ($("select#size").val()=== "Large") {
+//         var cost1= 3000;
+//       }
+//       else{alert("waaa")}
+
+
+//       var inputtedCrust = $("select#crust").val();
+
+//       if ($("select#crust").val()=="FlatBread Crust" ){
+//         var cost2= 500;
+//       } else if ($("select#crust").val() === "Custom Crust") {
+//         var cost2= 400;
+//       } else if ($("select#crust").val() === "Thin Crust") {
+//         var cost2= 800;
+//       }
+//       else{alert("waaa")}
+//       var inputtedTopping = $("select#topping").val();
+//       if ($("select#topping").val()=="Tomato" ){
+//         var cost3= 300;
+//       } else if ($("select#topping").val() === "Onions") {
+//         var cost3= 500;
+//       } else if ($("select#topping").val() === "Mushroom") {
+//         var cost3= 600;
+//       }
+//       else{alert("waaa")}
+//       var inputtedNumber = $("input#number").val();
+      
+//       var newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedTopping,inputtedNumber);
+     
+   
+     
+
+//         var newPrice=(parseInt(cost1)+parseInt(cost2)+parseInt(cost3))*inputtedNumber;
+       
+//         $("div#pizaa").append("<p> The price is :" + newPrice + "</span></p>");
+
+//         $("#show-chosen").show();
+
+//         $(".pizza-size").text( newPizza.sizeName);
+        
+//         $(".pizza-crust").text( newPizza.crustName);
+//         $(".pizza-topping").text( newPizza.toppingName);
+//         $(".pizza-number").text( newPizza.numberName);
+//         console.log(num);
+
+      
+     
+      
+//       });
+//       $(document).ready(function() {
+  
+//         $("#delivery").click(function() {
+//           // $("#gowith").show()
+//           // alert("The Price to deliver your pizza is "+ price);
+//           $(".button").hide()
+//           $(".form3").show()
+
+//             var price=1000;
+//            alert("The Price to deliver your pizza is " +price);
+//            var person = prompt("Please enter your name","laetitia");
+//            var location= prompt("Please enter your location", "kicukiro");
+//            alert("Thank you for choosing us"+ person+"\n"+"your pizza will be delivered at "+location)
+            
+//         });
+//       });    
+ 
+//       $(document).ready(function() {
+       
+  
+//         $("#checkout").click(function() {
+         
+//           if ( $("select#size").val()=== "Small") {
+           
+//           var price=1000;
+//            var cost1 = 1000;
+//             console.log(cost1);
+            
+//           } else if ($("select#size").val() === "Medium") {
+//              var price=1000;
+//            var cost1 = 2000;
+           
+//           } else if ($("select#size").val()=== "Large") {
+//              var price=1000;
+//             var cost1= 3000;
+//           }
+//           else{alert("waaa")}
+    
+    
+//           if ($("select#crust").val()=="FlatBread Crust" ){
+//             var price=1000;
+//             var cost2= 500;
+//           } else if ($("select#crust").val() === "Custom Crust") {
+//             var price=1000;
+//             var cost2= 400;
+//           } else if ($("select#crust").val() === "Thin Crust") {
+//             var cost2= 800;
+//           }
+//           else{alert("waaa")}
+        
+//           if ($("select#topping").val()=="Tomato" ){
+//             var price=1000;
+//             var cost3= 300;
+//           } else if ($("select#topping").val() === "Onions") {
+//             var price=1000;
+//             var cost3= 500;
+//           } else if ($("select#topping").val() === "Mushroom") {
+//             var price=1000;
+//             var cost3= 600;
+//           }
+//           else{alert("waaa")}
+          
+//           // var newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedTopping );
+         
+       
+         
+    
+//           //   var newPrice=parseInt(cost1)+parseInt(cost2)+parseInt(cost3);
+//           var newTotal=parseInt(cost1)+parseInt(cost2)+parseInt(cost3)+parseInt(price);
+//           console.log("sagjilsusio;u8q")
+//        alert(newTotal);
+//         });
+          
+//       });    
+// });
+//  resetFields();
+//   });
+function Pizza(size, crust, topping,number) {
+  this.sizeName= size;
+  this.crustName= crust;
+  this.toppingName= topping;
+  this.numberName= number;
 }
-  $(document).ready(function() {
+
+Pizza.prototype.fullFilled = function() {
+  return this.sizeName+ " " + this.crustName+ "" + this.toppingName+"" +this.numberName;
+}
+
   
-    $("#order").click(function() {
-       
+function resetFields() {
+  $("input#size").val("");
+  $("input#crust").val("");
+  $("input#topping").val("");
   
-    
-    $("form#formid").submit(function(event) {
-      event.preventDefault();
-   
-      var inputtedSize = $("select#size").val();
-      if ( $("select#size").val()=== "Small") {
-       
-       
-       var cost1 = 1000;
-        console.log(cost1);
-        
-      } else if ($("select#size").val() === "Medium") {
-       var cost1 = 2000;
-      } else if ($("select#size").val()=== "Large") {
-        var cost1= 3000;
-      }
-      else{alert("waaa")}
+}
+$(document).ready(function() {
 
-
-      var inputtedCrust = $("select#crust").val();
-
-      if ($("select#crust").val()=="FlatBread Crust" ){
-        var cost2= 500;
-      } else if ($("select#crust").val() === "Custom Crust") {
-        var cost2= 400;
-      } else if ($("select#crust").val() === "Thin Crust") {
-        var cost2= 800;
-      }
-      else{alert("waaa")}
-      var inputtedTopping = $("select#topping").val();
-      if ($("select#topping").val()=="Tomato" ){
-        var cost3= 300;
-      } else if ($("select#topping").val() === "Onions") {
-        var cost3= 500;
-      } else if ($("select#topping").val() === "Mushroom") {
-        var cost3= 600;
-      }
-      else{alert("waaa")}
-      var inputtedNumber = $("input#number").val();
-      
-      var newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedTopping,inputtedNumber);
-     
-   
+  $("#order").click(function() {
      
 
-        var newPrice=(parseInt(cost1)+parseInt(cost2)+parseInt(cost3))*inputtedNumber;
-       
-        $("div#pizaa").append("<p> The price is :" + newPrice + "</span></p>");
-
-        $("#show-chosen").show();
-
-        $(".pizza-size").text( newPizza.sizeName);
-        
-        $(".pizza-crust").text( newPizza.crustName);
-        $(".pizza-topping").text( newPizza.toppingName);
-        $(".pizza-number").text( newPizza.numberName);
-        console.log(num);
-
-      
-     
-      
-      });
-      $(document).ready(function() {
   
-        $("#delivery").click(function() {
-          // $("#gowith").show()
-          // alert("The Price to deliver your pizza is "+ price);
-          $(".button").hide()
-          $(".form3").show()
-
-            var price=1000;
-           alert("The Price to deliver your pizza is " +price);
-           var person = prompt("Please enter your name","laetitia");
-           var location= prompt("Please enter your location", "kicukiro");
-           alert("Thank you for choosing us"+ person+"\n"+"your pizza will be delivered at "+ location)
-            
-        });
-      });    
+  $("form#formid").submit(function(event) {
+    event.preventDefault();
  
-      $(document).ready(function() {
+    var inputtedSize = $("select#size").val();
+    if ( $("select#size").val()=== "Small") {
+     
+     
+     var cost1 = 1000;
+      console.log(cost1);
+      
+    } else if ($("select#size").val() === "Medium") {
+     var cost1 = 2000;
+    } else if ($("select#size").val()=== "Large") {
+      var cost1= 3000;
+    }
+    else{alert("waaa")}
+
+
+    var inputtedCrust = $("select#crust").val();
+
+    if ($("select#crust").val()=="FlatBread Crust" ){
+      var cost2= 500;
+    } else if ($("select#crust").val() === "Custom Crust") {
+      var cost2= 400;
+    } else if ($("select#crust").val() === "Thin Crust") {
+      var cost2= 800;
+    }
+    else{alert("waaa")}
+    var inputtedTopping = $("select#topping").val();
+    if ($("select#topping").val()=="Tomato" ){
+      var cost3= 300;
+    } else if ($("select#topping").val() === "Onions") {
+      var cost3= 500;
+    } else if ($("select#topping").val() === "Mushroom") {
+      var cost3= 600;
+    }
+    else{alert("waaa")}
+    var inputtedNumber = $("input#number").val();
+    
+    var newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedTopping,inputtedNumber);
+   
+ 
+   
+
+      var newPrice=(parseInt(cost1)+parseInt(cost2)+parseInt(cost3))*inputtedNumber;
+     
+      $("div#pizaa").append("<p> The price is :" + newPrice + "</span></p>");
+
+      $("#show-chosen").show();
+
+      $(".pizza-size").text( newPizza.sizeName);
+      
+      $(".pizza-crust").text( newPizza.crustName);
+      $(".pizza-topping").text( newPizza.toppingName);
+      $(".pizza-number").text( newPizza.numberName);
+    
+    });
+    $(document).ready(function() {
+
+      $("#delivery").click(function() {
+        // $("#gowith").show()
+        // alert("The Price to deliver your pizza is "+ price);
+        $(".button").hide()
+        
+
+          var price=1000;
+         alert("The Price to deliver your pizza is " +price);
+         var person = prompt("Please enter your name","laetitia");
+         var location= prompt("Please enter your location", "kicukiro");
+         
+         alert("Thank you for choosing us"+ person+"\n"+"your pizza will be delivered at: "+ location)
+          
+      });
+    });    
+
+    $(document).ready(function() {
+     
+
+      $("#checkout").click(function() {
+       
+        if ( $("select#size").val()=== "Small") {
+         
+        var price=1000;
+         var cost1 = 1000;
+          console.log(cost1);
+          
+        } else if ($("select#size").val() === "Medium") {
+           var price=1000;
+         var cost1 = 2000;
+         
+        } else if ($("select#size").val()=== "Large") {
+           var price=1000;
+          var cost1= 3000;
+        }
+        else{alert("waaa")}
+  
+  
+        if ($("select#crust").val()=="FlatBread Crust" ){
+          var price=1000;
+          var cost2= 500;
+        } else if ($("select#crust").val() === "Custom Crust") {
+          var price=1000;
+          var cost2= 400;
+        } else if ($("select#crust").val() === "Thin Crust") {
+          var cost2= 800;
+        }
+        else{alert("waaa")}
+      
+        if ($("select#topping").val()=="Tomato" ){
+          var price=1000;
+          var cost3= 300;
+        } else if ($("select#topping").val() === "Onions") {
+          var price=1000;
+          var cost3= 500;
+        } else if ($("select#topping").val() === "Mushroom") {
+          var price=1000;
+          var cost3= 600;
+        }
+        else{alert("waaa")}
+        
+        // var newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedTopping );
+       
+     
        
   
-        $("#checkout").click(function() {
-         
-          if ( $("select#size").val()=== "Small") {
-           
-          var price=1000;
-           var cost1 = 1000;
-            console.log(cost1);
-            
-          } else if ($("select#size").val() === "Medium") {
-             var price=1000;
-           var cost1 = 2000;
-           
-          } else if ($("select#size").val()=== "Large") {
-             var price=1000;
-            var cost1= 3000;
-          }
-          else{alert("waaa")}
-    
-    
-          if ($("select#crust").val()=="FlatBread Crust" ){
-            var price=1000;
-            var cost2= 500;
-          } else if ($("select#crust").val() === "Custom Crust") {
-            var price=1000;
-            var cost2= 400;
-          } else if ($("select#crust").val() === "Thin Crust") {
-            var cost2= 800;
-          }
-          else{alert("waaa")}
+        //   var newPrice=parseInt(cost1)+parseInt(cost2)+parseInt(cost3);
+
+
+
+
         
-          if ($("select#topping").val()=="Tomato" ){
-            var price=1000;
-            var cost3= 300;
-          } else if ($("select#topping").val() === "Onions") {
-            var price=1000;
-            var cost3= 500;
-          } else if ($("select#topping").val() === "Mushroom") {
-            var price=1000;
-            var cost3= 600;
-          }
-          else{alert("waaa")}
-          
-          // var newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedTopping );
-         
-       
-         
+        var inputtedNumber = $("input#number").val();
     
-          //   var newPrice=parseInt(cost1)+parseInt(cost2)+parseInt(cost3);
-          var newTotal=parseInt(cost1)+parseInt(cost2)+parseInt(cost3)+parseInt(price);
-          console.log("sagjilsusio;u8q")
-       alert(newTotal);
-        });
-          
-      });    
+        var newTotal=((parseInt(cost1)+parseInt(cost2)+parseInt(cost3))*inputtedNumber+parseInt(price));
+        
+     alert("the total amount you have to pay are :"+newTotal+"frws")
+     alert(inputtedZise)
+    //  $("div#pizaaa").append("<p> The price is :" + newTotal + "</span></p>");
+     
+    //  $("#pizaaa").append("<p> The price is :" + newTotal + "</span></p>");
+    
+      });
+        
+    });    
 });
- resetFields();
-  });
+resetFields();
+});
+
+
+function cycleBackgrounds() {
+  var index = 0;
+
+  $imageEls = $('.container .slide'); // Get the images to be cycled.
+
+  setInterval(function () {
+      // Get the next index.  If at end, restart to the beginning.
+      index = index + 1 < $imageEls.length ? index + 1 : 0;
+      
+      // Show the next
+      $imageEls.eq(index).addClass('show');
+      
+      // Hide the previous
+      $imageEls.eq(index - 1).removeClass('show');
+  }, 2000);
+};
+
+// Document Ready.
+$(function () {
+  cycleBackgrounds();
+});
